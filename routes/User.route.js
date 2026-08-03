12d754816c625f391305b0dbb6d4c659cb2f3727
Greesh Sutar <girishasutar223@gmail.com>
2026-08-03 14:24:11 +0000

    Aug 3, 2026, 7:54 PM

--- routes/User.route.js
+++ routes/User.route.js
@@ -4,14 +4,8 @@ const authnticatingUser = require("../validate/auth.middlware.js");
 
 function allroutes(app){
 console.log("login:", login);
-console.log("profile:", profile);
-console.log("auth:", authnticatingUser);
 
-console.log(typeof login);
-console.log(typeof profile);
-console.log(typeof authnticatingUser);
  
-console.log(authnticatingUser);
 app.post("/login",  login);
 
 app.get("/profile", authnticatingUser,   profile);
