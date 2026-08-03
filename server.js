caae284b73b2d46d9482f8ebeeb518b06a1d4bab
Greesh Sutar <girishasutar223@gmail.com>
2026-08-03 14:00:55 +0000

    Aug 3, 2026, 7:30 PM

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ let bcrypt = require("bcrypt");
 let express = require("express")
 let app = express();
 let env = require("dotenv");
+let ors = require("cors");
 const allroutes = require("./routes/User.route.js");
 env.config();
 
