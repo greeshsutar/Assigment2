cf5f0a58c80af5bf835fa9a3a122604b31876336
Greesh Sutar <girishasutar223@gmail.com>
2026-08-03 13:36:04 +0000

    Aug 3, 2026, 7:06 PM

--- config/User.config.js
+++ config/User.config.js
@@ -1,11 +1,11 @@
 let {Pool} = require("pool");
-
+let env = require("dotenv");
 const pool = new Pool({
 user:process.env.DB_USER,
-password:DB_PASSWORD,
-host:DB_HOST,
-port:DB_PORT,
-database:DB_NAME
+password:process.env.DB_PASSWORD,
+host:process.env.DB_HOST,
+port:process.env.DB_PORT,
+database:process.env.DB_NAME
 })
 
 module.exports = pool;
