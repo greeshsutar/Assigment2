3e14c84979643e501cd01ad85318577a8a493e75
Greesh Sutar <girishasutar223@gmail.com>
2026-08-03 13:44:50 +0000

    Aug 3, 2026, 7:14 PM

--- server.js
+++ server.js
@@ -3,13 +3,20 @@ let dotenv = require("dotenv");
 let bcrypt = require("bcrypt");
 let express = require("express")
 let app = express();
+let env = require("dotenv");
+env.config();
 
 app.use(express.json());
 
-
-
 app.use(cors());
 
-
 allroutes(app);
+
+
+app.listen(process.env,()=>{
+    console.log("Server Connect")
+})
+
+
+
 
